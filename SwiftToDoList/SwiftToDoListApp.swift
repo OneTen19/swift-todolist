edb9993f800e5fee809a20491564f7c2bb168aaf
OneTen19 <khy000426@naver.com>
2024-07-22 11:42:01 +0900

    Initial Commit

---
+++ SwiftToDoList/SwiftToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftToDoListApp.swift
+//  SwiftToDoList
+//
+//  Created by OneTen on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
