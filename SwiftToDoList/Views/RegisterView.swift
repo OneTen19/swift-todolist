a19491361db8670a810703f1763ee3c474bc11d8
OneTen19 <khy000426@naver.com>
2024-07-23 15:07:47 +0900

    [feat] Register View

--- SwiftToDoList/Views/RegisterView.swift
+++ SwiftToDoList/Views/RegisterView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack{
             // Header
             HeaderView(title: "Register", subTitle: "Start organizing todos", angle: -15, background: .orange)
             
-            
-            
+            Form {
+                TextField("Full Name", text: $name)
+                    .textFieldStyle(.plain)
+                    .autocorrectionDisabled()
+                
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(.plain)
+                    .textInputAutocapitalization(.none)
+                    .autocorrectionDisabled()
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(.plain)
+                
+                TLButton(title: "Create Account", background: .green) {
+                    //
+                }
+                
+            }
+            .offset(y: -50)
+
             
             Spacer()
         }
