4ef8cc53351363b6052aaccf2ac39b948995fb29
김한열 Hanyeol Kim <63261054+OneTen19@users.noreply.github.com>
2024-07-24 13:56:53 +0900

    [feat] Account UI (#20)

--- SwiftToDoList/Views/ToDoListView.swift
+++ SwiftToDoList/Views/ToDoListView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    @StateObject var viewModel = ToDoListViewViewModel()
+    private let userId: String
+    
+    init(userId: String){
+        self.userId = userId
+    }
+    
     var body: some View {
-        Text("To Do List View")
+        
+        
+        NavigationView(content: {
+            NavigationLink(destination: Text("Destination")) {
+                VStack{
+                    
+                }
+                .navigationTitle("To Do List")
+                .toolbar {
+                    Button {
+                        //
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+
+                }
+            }
+        })
+        
+        
     }
 }
 
 #Preview {
-    ToDoListView()
+    ToDoListView(userId: "")
 }
