9bdd86039e32c8163641ced4f0755c1dd9b31eb3
김한열 Hanyeol Kim <63261054+OneTen19@users.noreply.github.com>
2024-07-25 14:54:58 +0900

    [feat] Items List (#23)

--- SwiftToDoList/ViewModels/ToDoListViewViewModel.swift
+++ SwiftToDoList/ViewModels/ToDoListViewViewModel.swift
@@ -10,8 +10,10 @@ import Foundation
 class ToDoListViewViewModel : ObservableObject {
     @Published var showingNewItemView = false
     
-    
     init() {}
     
+    func delete(id: String){
+        
+    }
     
 }
---
+++ SwiftToDoList/Views/ToDoListItemView.swift
@@ -0,0 +1,46 @@
+//
+//  ToDoListItemView.swift
+//  SwiftToDoList
+//
+//  Created by OneTen on 7/22/24.
+//
+
+import SwiftUI
+
+struct ToDoListItemView: View {
+    @StateObject var viewModel = ProfileViewViewModel()
+    let item: ToDoListItem
+    
+    var body: some View {
+        HStack{
+            VStack(alignment: .leading){
+                Text(item.title)
+                    .font(.body)
+                
+                Text("\(Date(timeIntervalSince1970: item.dueDate).formatted(date: .abbreviated, time: .shortened))")
+                    .font(.footnote)
+                    .foregroundStyle(Color(.secondaryLabel))
+                
+            }
+            Spacer()
+            
+            
+            Button {
+                viewModel.toggleIsDone(item: item)
+            } label: {
+                Image(systemName: item.isDone ? "checkmark.circle.fill" : "circle")
+            }
+
+            
+        }
+    }
+}
+
+#Preview {
+    ToDoListItemView(item: .init(
+        id: "123",
+        title: "Get Milk",
+        dueDate: Date().timeIntervalSince1970,
+        createdDate: Date().timeIntervalSince1970,
+        isDone: true))
+}
